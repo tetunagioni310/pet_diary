784c51127b3fc24810ff335fc28dd66d07ac5fb8
tetunagioni310 <104610603+tetunagioni310@users.noreply.github.com>
2022-12-25 08:29:00 +0000

    [Update]error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # ゲストログインをログアウトする時は以下を無効にする
-  before_action :ensure_normal_customer, only: [:destroy]
+  # before_action :ensure_normal_customer, only: [:destroy]
 
   # ゲストログイン中にはセッションを消去できない
   def ensure_normal_customer
