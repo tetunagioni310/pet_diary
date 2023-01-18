1fe2c316d13d609f11c8e9fe68d77af2b105b48b
EC2 Default User <ec2-user@ip-172-31-14-119.ap-northeast-1.compute.internal>
2023-01-18 22:37:59 +0000

    [Update]top-page

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,7 +3,8 @@ class Public::HomesController < ApplicationController
   def top
     # 画像がついている投稿だけ取り出したい
     @posts = Post.joins(:customer).where(customers: { status: 1 }).order(id: :desc).limit(5)
-    @random_posts = Post.joins(:customer).where(customers: { status: 1 }).sample(10)
+    rand = Rails.env.production? ? "RANDOM()" : "rand()"
+    @random_posts = Post.joins(:customer).where(customers: { status: 1 }).order(rand).limit(10)
   end
 
   def about
