e26da08277ad118516b83f7b0ac060d13b0cfe7e
EC2 Default User <ec2-user@ip-172-31-14-119.ap-northeast-1.compute.internal>
2023-01-18 23:15:17 +0000

    [update]top-page test

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,8 @@
 class Public::HomesController < ApplicationController
 
   def top
-    # 画像がついている投稿だけ取り出したい
     @posts = Post.joins(:customer).where(customers: { status: 1 }).order(id: :desc).limit(5)
-    @order_posts = Post.joins(:customer).where(customers: { status: 1 }).sample(10)
+    @order_posts = Post.joins(:customer).where(customers: { status: 1 })
   end
 
   def about
