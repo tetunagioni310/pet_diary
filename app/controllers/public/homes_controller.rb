ad6f8f2c7e50a332c9c622e50c3524d62c5a6dee
EC2 Default User <ec2-user@ip-172-31-14-119.ap-northeast-1.compute.internal>
2023-01-20 13:19:47 +0000

    [Update]top-page

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,8 +2,8 @@ class Public::HomesController < ApplicationController
 
   def top
     @posts = Post.joins(:customer).where(customers: { status: 1 }).order(id: :desc).limit(5)
-    # @order_posts = Post.joins(:customer).where(customers: { status: 1 }).sample(10)
-    @order_posts = Post.joins(:customer).where(customers: { status: 1 }).where('(id & 255) = ?', rand(256)).order(Arel.sql('RANDOM')).limit(10)
+    # @order_posts = Post.joins(:customer).where(customers: { status: 1 }).where('(id & 255) = ?', rand(256)).order(Arel.sql('RANDOM')).limit(10)
+    @order_posts = Post.joins(:customer).where(customers: { status: 1 })
   end
 
   def about
