08d3e73889fb2475aee2fe1bb16b77ca5caaa7b6
EC2 Default User <ec2-user@ip-172-31-14-119.ap-northeast-1.compute.internal>
2023-01-24 10:16:25 +0000

    [Update]top-page post-image-list

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,7 +4,8 @@ class Public::HomesController < ApplicationController
     @posts = Post.joins(:customer).where(customers: { status: 1 }).order(id: :desc).limit(5)
     
     # @order_posts = Post.joins(:customer).where(customers: { status: '1' }).where('(posts.id & 255) = ?', rand(1..256)).limit(10)
-    @order_posts = Post.joins(:customer).where(customers: { status: '1' }).order(Arel.sql('RANDOM()')).limit(10)
+     @order_posts = Post.joins(:customer).where(customers: { status: '1' })
+    # @order_posts = Post.joins(:customer).where(customers: { status: '1' }).order(Arel.sql('RANDOM()')).limit(10)
 
     # @released_posts = Post.joins(:customer).where(customers: { status: 1 })
     # @order_posts = @released_posts.where('(id & 255) = ?', rand(256)).order(Arel.sql('RANDOM()')).limit(8)
