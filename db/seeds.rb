750600c6a7ffc4aeef24345d5db67d64e1c7118b
EC2 Default User <ec2-user@ip-172-31-14-119.ap-northeast-1.compute.internal>
2023-01-25 12:07:28 +0000

    [Update]post_all partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,52 +27,52 @@ Customer.create!(
    password: 'testtesttest1105'
 )
 
-# 20.times do |n|
-#  Customer.create!(
-#   nick_name: Faker::Internet.user_name,
-#   email:     Faker::Internet.unique.email,
-#   password:  "passwordpassword")
-# end
+20.times do |n|
+ Customer.create!(
+  nick_name: Faker::Internet.user_name,
+  email:     Faker::Internet.unique.email,
+  password:  "passwordpassword")
+end
 
-# Customer.all.each do |customer|
-#  Pet.create!(
-#   customer_id: customer.id,
-#   group_id: 1,
-#   # group_id が3の時だけ作成したい
-#   pet_kind: Faker::Creature::Dog.breed,
-#   pet_name: Faker::Creature::Dog.name,
-#   gender:   rand(1..2),
-#   birthday: Faker::Date.between_except( from: '2014-09-23', to: '2020-09-25', excepted: '2015-01-24')
-#   )
+Customer.all.each do |customer|
+ Pet.create!(
+  customer_id: customer.id,
+  group_id: 1,
+  # group_id が3の時だけ作成したい
+  pet_kind: Faker::Creature::Dog.breed,
+  pet_name: Faker::Creature::Dog.name,
+  gender:   rand(1..2),
+  birthday: Faker::Date.between_except( from: '2014-09-23', to: '2020-09-25', excepted: '2015-01-24')
+  )
   
-#   Pet.create!(
-#   customer_id: customer.id,
-#   group_id: 2,
-#   pet_kind: Faker::Creature::Cat.breed,
-#   pet_name: Faker::Creature::Cat.name,
-#   gender:   rand(1..2),
-#   birthday: Faker::Date.between_except( from: '2014-09-23', to: '2020-09-25', excepted: '2015-01-24')
-#   )
+  Pet.create!(
+  customer_id: customer.id,
+  group_id: 2,
+  pet_kind: Faker::Creature::Cat.breed,
+  pet_name: Faker::Creature::Cat.name,
+  gender:   rand(1..2),
+  birthday: Faker::Date.between_except( from: '2014-09-23', to: '2020-09-25', excepted: '2015-01-24')
+  )
   
-#   Pet.create!(
-#   customer_id: customer.id,
-#   group_id: 3,
-#   pet_kind: Faker::Creature::Animal.name,
-#   pet_name: Faker::Creature::Dog.name,
-#   gender:   rand(1..2),
-#   birthday: Faker::Date.between_except( from: '2014-09-23', to: '2020-09-25', excepted: '2015-01-24')
-#   )
+  Pet.create!(
+  customer_id: customer.id,
+  group_id: 3,
+  pet_kind: Faker::Creature::Animal.name,
+  pet_name: Faker::Creature::Dog.name,
+  gender:   rand(1..2),
+  birthday: Faker::Date.between_except( from: '2014-09-23', to: '2020-09-25', excepted: '2015-01-24')
+  )
  
-#   customer.pets.each do |pet|
-#    Post.create!(
-#     customer_id: customer.id,
-#     pet_id: pet.id,
-#     post_title: Faker::Lorem.sentence,
-#     post_content: Faker::Lorem.sentence,
-#     post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/sample-image.jpg')),filename: 'sample-image.jpg')
-#    )
-#   end
-# end
+  customer.pets.each do |pet|
+   Post.create!(
+    customer_id: customer.id,
+    pet_id: pet.id,
+    post_title: Faker::Lorem.sentence,
+    post_content: Faker::Lorem.sentence,
+    post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join('app/assets/images/sample-image.jpg')),filename: 'sample-image.jpg')
+   )
+  end
+end
 
 # customers = Customer.all
 # customer  = customers.first
