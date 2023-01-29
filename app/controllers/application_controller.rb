c943214d1a2dd116329964847309cab92b81c22e
EC2 Default User <ec2-user@ip-172-31-14-119.ap-northeast-1.compute.internal>
2023-01-29 22:40:32 +0000

    [Update]gestuser after_login_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
 
-
-
   def guest_sign_in
     customer = Customer.guest
     sign_in customer
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
+    redirect_to public_customer_path(customer.id), notice: 'ゲストユーザーとしてログインしました'
   end
 
 end
