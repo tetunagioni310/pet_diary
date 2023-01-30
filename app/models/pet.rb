05b65685f0d033b8b35240f27cae63eb433ab9cd
EC2 Default User <ec2-user@ip-172-31-14-119.ap-northeast-1.compute.internal>
2023-01-30 13:24:39 +0000

    [Update]layout pet.rb validate

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,7 +4,7 @@ class Pet < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :works, dependent: :destroy
 
-  validates :pet_name, presence: true
+  validates :pet_name, presence: true,length: { maximum: 8 }
   validates :group_id, presence: true
   validates :gender, presence: true
   validates :birthday, presence: true
